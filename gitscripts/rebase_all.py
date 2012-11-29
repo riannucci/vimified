eb72ed0230b9073e32ae62f199420f267273da60
Robert A. Iannucci Jr <robbie@rail.com>
2012-11-29 15:41:53 -0800

    Improve the error handling a bit

--- gitscripts/rebase_all.py
+++ gitscripts/rebase_all.py
@@ -3,17 +3,32 @@ import subprocess
 import sys
 
 
+# Exception classes used by this module.
+class CalledProcessError(Exception):
+  def __init__(self, returncode, cmd, output=None, out_err=None):
+    super(CalledProcessError).__init__(self)
+    self.returncode = returncode
+    self.cmd = cmd
+    self.output = output
+    self.out_err = out_err
+
+  def __str__(self):
+    return (
+        'Command "%s" returned non-zero exit status %d' %
+        (self.cmd, self.returncode))
+
+
 def check_output(*popenargs, **kwargs):
   if 'stdout' in kwargs:
     raise ValueError('stdout argument not allowed, it will be overridden.')
   process = subprocess.Popen(stdout=subprocess.PIPE, *popenargs, **kwargs)
-  output, unused_err = process.communicate()
+  output, out_err = process.communicate()
   retcode = process.poll()
   if retcode:
     cmd = kwargs.get('args')
     if cmd is None:
       cmd = popenargs[0]
-    raise subprocess.CalledProcessError(retcode, cmd, output=output)
+    raise CalledProcessError(retcode, cmd, output=output, out_err=out_err)
   return output
 
 
@@ -35,13 +50,13 @@ def main():
       remote = run(('git', 'config', 'branch.'+branch+'.remote'))
       if remote != '.':
         continue
-    except subprocess.CalledProcessError:
+    except CalledProcessError:
       continue
 
     try:
       parent = run(('git', 'config', 'branch.'+branch+'.merge'))
       parent = run(('git', 'rev-parse', '--abbrev-ref', parent))
-    except subprocess.CalledProcessError:
+    except CalledProcessError:
       parent = None
     branch_tree[branch] = parent
 
@@ -49,7 +64,12 @@ def main():
   while branch_tree:
     for branch in (b for b, p in branch_tree.items() if p not in branch_tree):
       run(('git', 'checkout', branch))
-      run(('git', 'rebase'))
+      try:
+        run(('git', 'rebase'))
+      except CalledProcessError as ex:
+        print ex.output
+        print ex.out_err
+        raise
       del branch_tree[branch]
 
   run(('git', 'checkout', orig_branch))
