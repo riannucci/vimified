402c4807475f4fb8dd6c83f5e72ff694167983b4
Robert Iannucci <robbie@rail.com>
2013-05-09 17:13:09 -0700

    Make pylog highlight HEAD lines awesomely

--- gitscripts/pylog.py
+++ gitscripts/pylog.py
@@ -7,6 +7,7 @@ def main():
   BRIGHT = '\x1b[1m'
   CYAN = '\x1b[36m'
   GREEN = '\x1b[32m'
+  BLUEBAK = '\x1b[44m'
   MAGENTA = '\x1b[35m'
   RED = '\x1b[31m'
   RESET = '\x1b[m'
@@ -22,9 +23,14 @@ def main():
       if start != -1 and end != -1:
         start += len(GREEN) + 2
         branch_list = line[start:end].split(', ')
+        branches_str = ''
         if branch_list:
           colored_branches = []
+          head_marker = ''
           for b in branch_list:
+            if b == "HEAD":
+              head_marker = BLUEBAK+BRIGHT+'*'
+              continue
             if b == current:
               colored_branches.append(CYAN+BRIGHT+b+RESET)
               current = None
@@ -36,11 +42,11 @@ def main():
               all_tags.remove(b)
             else:
               colored_branches.append(RED+b)
-          line = "%s%s%s" % (line[:start],
-                              (GREEN+", ").join(colored_branches)+GREEN,
-                              line[end:])
+            branches_str = '(%s) ' % ((GREEN+", ").join(colored_branches)+GREEN)
+          line = "%s%s%s" % (line[:start-1], branches_str, line[end+5:])
+          if head_marker:
+            line = line.replace('*', head_marker, 1)
       sys.stdout.write(line)
-
   except KeyboardInterrupt:
     pass
   except IOError:
